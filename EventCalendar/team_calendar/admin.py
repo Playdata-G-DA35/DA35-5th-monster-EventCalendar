f29b98901762e3239c8fc6d65c18c1560a32df37
이서희 <162577851+dawnhub@users.noreply.github.com>
2024-07-08 00:16:52 +0900

    Delete EventCalendar directory

--- EventCalendar/team_calendar/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Team, CalendarData
-
-# Register your models here.
-admin.site.register(Team)
-admin.site.register(CalendarData)
--- EventCalendar/team_calendar/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TeamCalendarConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'team_calendar'
