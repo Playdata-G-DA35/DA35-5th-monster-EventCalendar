db01a35e233422cbabdbc1c68843b7141a53abdd
이서희 <162577851+dawnhub@users.noreply.github.com>
2024-07-07 23:21:24 +0900

    Add files via upload

---
+++ EventCalendar/account/urls.py
@@ -0,0 +1,26 @@
+# account/urls.py
+
+from django.urls import path
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
+from . import views
+
+app_name = "account"
+urlpatterns = [
+    path("join", views.create, name="join"),
+    # path("login", views.user_login, name="login"),
+    path("login", 
+        LoginView.as_view(
+            template_name="account/login.html", # login form template
+            form_class=AuthenticationForm       # Form class
+        ),
+        name="login"),
+    path("logout", views.user_logout, name="logout"),
+    path("detail", views.user_detail, name="detail"), 
+    path("password_change", views.change_password, name="password_change"),
+    path("update", views.user_update, name="update"),
+    path("delete", views.user_delete, name="deletle"),
+    path("team_delete/<int:user_id>", views.user_team_delete, name="team_delete"),
+    path("approve/<int:user_id>", views.approve_request, name="approve_request"),
+    path("reject/<int:user_id>", views.reject_request, name="reject_request")
+]
